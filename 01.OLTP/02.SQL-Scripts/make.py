434f2cd6262809a553490d0af05fbf773166c75b
Antonio Aguilar <jaguilar992@gmail.com>
2018-07-21 10:22:14 -0600

    Init

---
+++ 01.OLTP/02.SQL-Scripts/make.py
@@ -0,0 +1,55 @@
+# categoria_producto
+"https://api.mockaroo.com/api/4cb8da40?count=1000&key=a87680d0"
+# ciudad
+"https://api.mockaroo.com/api/a8314890?count=80&key=a87680d0"
+# cliente
+"https://api.mockaroo.com/api/a844f860?count=1000&key=a87680d0"
+# departamento
+"https://api.mockaroo.com/api/b4d5e780?count=20&key=a87680d0"
+# descuento
+"https://api.mockaroo.com/api/7ac9d950?count=1000&key=a87680d0"
+# descuento_factura
+"https://api.mockaroo.com/api/34cded00?count=1000&key=a87680d0" 
+# detalle_factura
+"https://api.mockaroo.com/api/2e758e90?count=1000&key=a87680d0"
+# detalle_movimiento
+"https://api.mockaroo.com/api/da6e7e10?count=1000&key=a87680d0"
+# empleado
+"https://api.mockaroo.com/api/2f4d63a0?count=1000&key=a87680d0"
+# estante
+"https://api.mockaroo.com/api/57df4500?count=1000&key=a87680d0"
+# factura
+"https://api.mockaroo.com/api/96278610?count=1000&key=a87680d0"
+# forma_pago
+"https://api.mockaroo.com/api/714ebf20?count=1000&key=a87680d0"
+# impuesto
+"https://api.mockaroo.com/api/2535d6a0?count=1000&key=a87680d0"
+# laboratorio
+"https://api.mockaroo.com/api/449c5430?count=1000&key=a87680d0"
+# lote
+"https://api.mockaroo.com/api/800962b0?count=1000&key=a87680d0"
+# lote_estante
+"https://api.mockaroo.com/api/6eed7a20?count=1000&key=a87680d0"
+# medicamentos
+"https://api.mockaroo.com/api/784f4bf0?count=1000&key=a87680d0"
+# movimiento_producto
+"https://api.mockaroo.com/api/2cae1920?count=1000&key=a87680d0"
+# pais
+"https://api.mockaroo.com/api/d1862bc0?count=8&key=a87680d0"
+# persona
+"https://api.mockaroo.com/api/af19d180?count=1000&key=a87680d0"
+# presentacion
+"https://api.mockaroo.com/api/25b14740?count=1000&key=a87680d0"
+# producto
+"https://api.mockaroo.com/api/923e2d20?count=1000&key=a87680d0"
+# sucursal
+"https://api.mockaroo.com/api/2b4b13c0?count=1000&key=a87680d0"
+# telefono
+"https://api.mockaroo.com/api/bdbb2b70?count=1000&key=a87680d0"
+# telefono_persona
+"https://api.mockaroo.com/api/1e9e6cc0?count=1000&key=a87680d0"
+# telefono_sucursal
+"https://api.mockaroo.com/api/13ad3f60?count=1000&key=a87680d0" 
+# tipo_movimiento_producto
+"https://api.mockaroo.com/api/2f299830?count=1000&key=a87680d0"
+
---
+++ 01.OLTP/02.SQL-Scripts/make.sh
@@ -0,0 +1,81 @@
+echo GET: categoria_producto
+curl "https://api.mockaroo.com/api/4cb8da40?count=1000&key=a87680d0"
+echo ------->
+echo GET: ciudad
+curl "https://api.mockaroo.com/api/a8314890?count=80&key=a87680d0"
+echo ------->
+echo GET: cliente
+curl "https://api.mockaroo.com/api/a844f860?count=1000&key=a87680d0"
+echo ------->
+echo GET: departamento
+curl "https://api.mockaroo.com/api/b4d5e780?count=20&key=a87680d0"
+echo ------->
+echo GET: descuento
+curl "https://api.mockaroo.com/api/7ac9d950?count=1000&key=a87680d0"
+echo ------->
+echo GET: descuento_factura
+curl "https://api.mockaroo.com/api/34cded00?count=1000&key=a87680d0" 
+echo ------->
+echo GET: detalle_factura
+curl "https://api.mockaroo.com/api/2e758e90?count=1000&key=a87680d0"
+echo ------->
+echo GET: detalle_movimiento
+curl "https://api.mockaroo.com/api/da6e7e10?count=1000&key=a87680d0"
+echo ------->
+echo GET: empleado
+curl "https://api.mockaroo.com/api/2f4d63a0?count=1000&key=a87680d0"
+echo ------->
+echo GET: estante
+curl "https://api.mockaroo.com/api/57df4500?count=1000&key=a87680d0"
+echo ------->
+echo GET: factura
+curl "https://api.mockaroo.com/api/96278610?count=1000&key=a87680d0"
+echo ------->
+echo GET: forma_pago
+curl "https://api.mockaroo.com/api/714ebf20?count=1000&key=a87680d0"
+echo ------->
+echo GET: impuesto
+curl "https://api.mockaroo.com/api/2535d6a0?count=1000&key=a87680d0"
+echo ------->
+echo GET: laboratorio
+curl "https://api.mockaroo.com/api/449c5430?count=1000&key=a87680d0"
+echo ------->
+echo GET: lote
+curl "https://api.mockaroo.com/api/800962b0?count=1000&key=a87680d0"
+echo ------->
+echo GET: lote_estante
+curl "https://api.mockaroo.com/api/6eed7a20?count=1000&key=a87680d0"
+echo ------->
+echo GET: medicamentos
+curl "https://api.mockaroo.com/api/784f4bf0?count=1000&key=a87680d0"
+echo ------->
+echo GET: movimiento_producto
+curl "https://api.mockaroo.com/api/2cae1920?count=1000&key=a87680d0"
+echo ------->
+echo GET: pais
+curl "https://api.mockaroo.com/api/d1862bc0?count=8&key=a87680d0"
+echo ------->
+echo GET: persona
+curl "https://api.mockaroo.com/api/af19d180?count=1000&key=a87680d0"
+echo ------->
+echo GET: presentacion
+curl "https://api.mockaroo.com/api/25b14740?count=1000&key=a87680d0"
+echo ------->
+echo GET: producto
+curl "https://api.mockaroo.com/api/923e2d20?count=1000&key=a87680d0"
+echo ------->
+echo GET: sucursal
+curl "https://api.mockaroo.com/api/2b4b13c0?count=1000&key=a87680d0"
+echo ------->
+echo GET: telefono
+curl "https://api.mockaroo.com/api/bdbb2b70?count=1000&key=a87680d0"
+echo ------->
+echo GET: telefono_persona
+curl "https://api.mockaroo.com/api/1e9e6cc0?count=1000&key=a87680d0"
+echo ------->
+echo GET: telefono_sucursal
+curl "https://api.mockaroo.com/api/13ad3f60?count=1000&key=a87680d0" 
+echo ------->
+echo GET: tipo_movimiento_producto
+curl "https://api.mockaroo.com/api/2f299830?count=1000&key=a87680d0"
+echo ------->
