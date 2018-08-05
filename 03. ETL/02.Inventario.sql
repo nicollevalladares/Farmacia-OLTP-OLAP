778c7c6e6c85f9dfa36ed5bac901938e3b5c9edf
EliteArmy <ariel9501@yahoo.com>
2018-08-04 19:34:35 -0600

    Trabajando en los ETL

--- 03. ETL/02.Inventario.sql
+++ 03. ETL/02.Inventario.sql
@@ -1,36 +1,29 @@
 /*Tabla Dimension: Lote*/
-SELECT 
-FROM
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-GROUP BY
-ORDER BY 
+SELECT lot.id_lote as id_lote, lot.lote as lote
+FROM lote lot 
+GROUP BY lot.id_lote
+ORDER BY lot.id_lote
 
 /*Tabla Dimension: Producto*/
-SELECT 
-FROM
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
+SELECT pro.id_producto as id_producto, pro.nombre as producto, 
+lot.id_lote as id_lote, lab.id_laboratorio as id_lab
+FROM producto pro
+INNER JOIN lote lot ON 
+INNER JOIN medicamentos med ON
+INNER JOIN laboratorio lab ON
 INNER JOIN 
 GROUP BY
 ORDER BY 
 
 /*Tabla Dimension: Laboratorio*/
-SELECT 
-FROM
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-GROUP BY
-ORDER BY 
+SELECT lab.id_laboratorio as id_lab, lab.nombre_laboratorio as Laboratorio
+FROM laboratorio lab
+GROUP BY lab.id_laboratorio
+ORDER BY lab.id_laboratorio
 
 /*Tabla Dimension: Lugar*/
-SELECT 
-FROM
+SELECT ???? as id_lugar, pai.pais as pais, ciu.ciudad as ciudad, suc.id_sucursal as sucursal
+FROM sucursal suc
 INNER JOIN 
 INNER JOIN 
 INNER JOIN 
@@ -39,7 +32,7 @@ GROUP BY
 ORDER BY 
 
 /*Tabla Dimension: Tiempo*/
-SELECT 
+SELECT as id_tiempo, as mes, as año, as semestre, as trimestre
 FROM
 INNER JOIN 
 INNER JOIN 
