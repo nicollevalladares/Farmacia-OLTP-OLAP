5289f54a872b1f86c83a2df1fb704ee339200371
EliteArmy <ariel9501@yahoo.com>
2018-08-04 20:44:33 -0600

    ETL Inventario

--- 03. ETL/02.Inventario.sql
+++ 03. ETL/02.Inventario.sql
@@ -6,29 +6,28 @@ ORDER BY lot.id_lote
 
 /*Tabla Dimension: Producto*/
 SELECT pro.id_producto as id_producto, pro.nombre as producto, 
-lot.id_lote as id_lote, lab.id_laboratorio as id_lab
+  lot.id_lote as id_lote, lab.id_laboratorio as id_lab
 FROM producto pro
-INNER JOIN lote lot ON 
-INNER JOIN medicamentos med ON
-INNER JOIN laboratorio lab ON
-INNER JOIN 
-GROUP BY
-ORDER BY 
+INNER JOIN lote lot ON pro.id_producto = lot.id_producto
+INNER JOIN medicamentos med ON pro.id_producto = med.id_producto
+INNER JOIN laboratorio lab  ON med.id_laboratorio = lab.id_laboratorio
+GROUP BY pro.id_producto
+ORDER BY pro.id_producto
 
 /*Tabla Dimension: Laboratorio*/
-SELECT lab.id_laboratorio as id_lab, lab.nombre_laboratorio as Laboratorio
+SELECT lab.id_laboratorio as id_lab, lab.nombre_laboratorio as laboratorio
 FROM laboratorio lab
 GROUP BY lab.id_laboratorio
 ORDER BY lab.id_laboratorio
 
 /*Tabla Dimension: Lugar*/
-SELECT ???? as id_lugar, pai.pais as pais, ciu.ciudad as ciudad, suc.id_sucursal as sucursal
+SELECT ???? as id_lugar, pai.pais as pais, ciu.ciudad as ciudad, 
+  suc.id_sucursal as sucursal
 FROM sucursal suc
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-INNER JOIN 
-GROUP BY
+INNER JOIN ciudad ciu ON suc.id_ciudad = ciu.id_ciudad
+INNER JOIN departamento dep ON ciu.id_departamento = dep.id_departamento 
+INNER JOIN pais pai ON dep.id_pais = pai.id_pais
+GROUP BY 
 ORDER BY 
 
 /*Tabla Dimension: Tiempo*/
