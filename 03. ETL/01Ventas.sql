2fb10bd211c3aabacdf75a86050b7982d5eed5ba
EliteArmy <ariel9501@yahoo.com>
2018-08-04 18:10:10 -0600

    Pequeño cambio

--- 03. ETL/01Ventas.sql
+++ 03. ETL/01Ventas.sql
@@ -1,6 +1,5 @@
 /*Tabla dimension: lugar*/
-SELECT
-	s.id_sucursal as id_lugar, s.nombre as sucursal, c.ciudad, d.departamento, p.pais
+SELECT s.id_sucursal as id_lugar, s.nombre as sucursal, c.ciudad, d.departamento, p.pais
 FROM detalle_factura df
 INNER JOIN factura f ON f.id_factura = df.id_factura
 INNER JOIN sucursal s ON s.id_sucursal = f.id_sucursal
@@ -8,5 +7,4 @@ INNER JOIN ciudad c ON c.id_ciudad = s.id_ciudad
 INNER JOIN departamento d ON d.id_departamento = c.id_departamento
 INNER JOIN pais p ON p.id_pais = d.id_pais
 GROUP BY s.id_sucursal, s.nombre, c.ciudad,d.departamento ,p.pais
-ORDER BY p.pais
-;
+ORDER BY p.pais;
