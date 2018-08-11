084dd74402a48717114b30cb8e321c6f3abfcad6
Androimeda <mnunezo@unah.hn>
2018-08-11 12:46:00 -0600

    DM_INVENTARIO

--- 02.OLAP/DataMarts/02.DDL_DM_INVENTARIO/DDL_INVENTARIO.sql
+++ 02.OLAP/DataMarts/02.DDL_DM_INVENTARIO/DDL_INVENTARIO.sql
@@ -1,6 +1,6 @@
 use DW_INVENTARIO;
 
-create table DIM_TIEMPO(
+create table DIM_TIEMPO_INVENTARIO(
 	id_tiempo int identity(1,1) not null,
 	mes int not null,
 	año int not null,
@@ -9,29 +9,29 @@ create table DIM_TIEMPO(
 	constraint tiempoPK primary key(id_tiempo)
 );
 
-create table DIM_LOTE(
+create table DIM_LOTE_INVENTARIO(
 	id_lote int not null,
 	lote varchar(100),
 	constraint lotePK primary key(id_lote)
 );
 
-create table DIM_LAB(
+create table DIM_LAB_INVENTARIO(
 	id_lab int not null,
 	laboratorio varchar(255) not null,
 	constraint labPK primary key(id_lab)
 );
 
-create table DIM_PRODUCTO(
+create table DIM_PRODUCTO_INVENTARIO(
 	id_producto int not null,
 	producto varchar(100) not null,
 	id_lab int not null,
 	id_lote int not null,
 	constraint productoPK primary key(id_producto),
-	constraint labFK foreign key(id_lab) references DIM_LAB(id_lab),
-	constraint loteFK foreign key(id_lote) references DIM_LOTE(id_lote)
+	constraint labFK foreign key(id_lab) references DIM_LAB_INVENTARIO(id_lab),
+	constraint loteFK foreign key(id_lote) references DIM_LOTE_INVENTARIO(id_lote)
 );
 
-create table DIM_LUGAR(
+create table DIM_LUGAR_INVENTARIO(
 	id_lugar int not null,
 	pais varchar(50) not null,
 	ciudad varchar(100) not null,
@@ -39,12 +39,12 @@ create table DIM_LUGAR(
 	constraint lugarPK primary key(id_lugar)
 );
 
-create table HECHOS_INVENTARIO(
+create table HECHOS_INVENTARIO_INVENTARIO(
 	cantidad int not null,
 	id_producto int not null,
 	id_tiempo int not null,
 	id_lugar int not null,
-	constraint productoFK foreign key(id_producto) references DIM_PRODUCTO(id_producto),
-	constraint tiempoFK foreign key(id_tiempo) references DIM_TIEMPO(id_tiempo),
-	constraint lugarFK foreign key(id_lugar) references DIM_LUGAR(id_lugar)
+	constraint productoFK foreign key(id_producto) references DIM_PRODUCTO_INVENTARIO(id_producto),
+	constraint tiempoFK foreign key(id_tiempo) references DIM_TIEMPO_INVENTARIO(id_tiempo),
+	constraint lugarFK foreign key(id_lugar) references DIM_LUGAR_INVENTARIO(id_lugar)
 );
